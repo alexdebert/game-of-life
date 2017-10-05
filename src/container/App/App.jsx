20422be94599693c4e0a774b0a87333e46bed0cc
Alexandre Debert <alexandre10.debert@gmail.com>
2017-10-05 00:06:55 -0300

    Modifying .gitignore Modifying App component Creating Grid component

--- src/container/App/App.jsx
+++ src/container/App/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return (
-      <div>
-          <h1>Game of Life</h1>
-      </div>
-  )
+import Grid from '../../container/Grid/Grid'
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state ={
+            generation : 0
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Game of Life</h1>
+                <Grid />
+                <h2>Generations: {this.state.generation}</h2>
+
+            </div>
+        )
+    }
 };
+
+App.PropTypes = {
+    generation : PropTypes.number
+};
+
 export default App;
---
+++ src/container/Grid/Grid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Grid extends React.Component {
+    render() {
+        return (
+            <div>
+                GRID
+            </div>
+        )
+    }
+}
+
+export default Grid
